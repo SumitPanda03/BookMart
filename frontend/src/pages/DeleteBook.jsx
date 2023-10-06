4eb0899cb53f4027e7b23207d6733cc74247e946
Sumit Panda <sumitpanda1308@gmail.com>
2023-10-06 17:38:13 +0530

    CORS Policy updated 5

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`https://book-mart-three.vercel.app/api/books/${id}`)
+      .delete(`https://book-mart-three.vercel.app/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
