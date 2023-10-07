0589a139f492dd5baa95d7bf73423aa72609afe8
Sumit Panda <sumitpanda1308@gmail.com>
2023-10-07 22:29:14 +0530

    Changed Site Title Name

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books')
+      .get('https://bookmart-bvuu.onrender.com/books')
       .then((response) => {
         setBooks(response.data);
         setLoading(false);
